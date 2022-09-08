fb539b2fe01eba5b540329efcc7e26af39f7aa6a
HYERIN0718 <lin7258@naver.com>
2022-09-08 13:45:04 +0900

    #10-db 인텔리제이 소스코드 새로운 프로젝트 재생성함
    이전 소스코드는 깃허브에 커밋이되지않아서 새로운 프로젝트를 다시 만들었음

---
+++ projectsourceboard/src/main/java/com/fastcampus/projectsourceboard/ProjectsourceboardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectsourceboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsourceboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsourceboardApplication.class, args);
+    }
+
+}
---
+++ projectsourceboard/src/test/java/com/fastcampus/projectsourceboard/ProjectsourceboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectsourceboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsourceboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
